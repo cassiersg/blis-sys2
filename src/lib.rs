2640cdd18cb13289110afded44045b2ee44083a5
Gaëtan Cassiers <gaetan.cassiers@iaik.tugraz.at>
2023-03-05 17:00:53 +0100

    Docs and upgrade to blis-src 0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! This is a binding for the C API for the [BLIS library](https://github.com/flame/blis).
+//!
+//! This crate depends on `blis-src` for the actual BLIS dependency.
+//! **You have to explicitly depend on [`blis-src`](https://docs.rs/blis-src)** to use this crate.
+//! The version of this crate matches the `blis-src` version, not the BLIS version.
+//!
+//! Related crates and alternatives:
+//! - [`blis-src`](https://docs.rs/blis-src): build/link BLIS library
+//! - [BLAS/LAPACK crate family](https://github.com/blas-lapack-rs/blas-lapack-rs.github.io/wiki)
+//! - [`blis-sys`](https://docs.rs/blis-sys): C binding for BLIS, includes its own BLIS build.
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
